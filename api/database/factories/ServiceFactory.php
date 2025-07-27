fbe50098ae6448ee6b1aea621bd5ea27d3679ed5
Saleh Ibne Omar <35599978+salehibneomar@users.noreply.github.com>
2025-07-28 00:35:42 +0600

    task: added ServiceFatory and Seeded 200 data as chunks, also type casted the enum status values to string since database enum stores as string (#7)

--- api/database/factories/ServiceFactory.php
+++ api/database/factories/ServiceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Enums\ServiceStatus;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
@@ -16,8 +17,16 @@ class ServiceFactory extends Factory
      */
     public function definition(): array
     {
+        $year = date('Y');
+        $start = "$year-01-01";
+        $end = "$year-12-31";
+
         return [
-            //
+            'name' => $this->faker->words(2, true),
+            'description' => $this->faker->optional()->paragraph(),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'status' => $this->faker->randomElement(ServiceStatus::values()),
+            'timeline' => $this->faker->date('Y-m-d', $this->faker->dateTimeBetween($start, $end)),
         ];
     }
 }
--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Database\Factories\AdminFactory;
 use Database\Factories\CustomerFactory;
+use Database\Factories\ServiceFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,12 @@ class DatabaseSeeder extends Seeder
             AdminFactory::new()->create();
         }
 
-        CustomerFactory::new()->count(50)->create();
+        // CustomerFactory::new()->count(10)->create();
+
+        $chunkSize = 50;
+        $total = 200;
+        for ($i = 0; $i < $total; $i += $chunkSize) {
+            ServiceFactory::new()->count($chunkSize)->create();
+        }
     }
 }
