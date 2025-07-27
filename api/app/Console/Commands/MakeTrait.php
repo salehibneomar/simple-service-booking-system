a9e9b9bf35c84716a3a6237b27983ec17075606f
salehibneomar <salehibneomar@gmail.com>
2025-07-27 19:24:15 +0600

    added custom command for creating traits

---
+++ api/app/Console/Commands/MakeTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeService extends GeneratorCommand
+{
+    protected $name = 'make:trait';
+    protected $description = 'Create a new trait';
+    protected $type = 'Trait';
+
+    protected function getStub()
+    {
+        return app_path('Console/Commands/stubs/trait.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Traits';
+    }
+}
