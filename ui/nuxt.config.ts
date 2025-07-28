30bb557c1bb450157ad6175b9eb3bfb33ebbf5ac
salehibneomar <salehibneomar@gmail.com>
2025-07-28 14:30:05 +0600

    feature/frontend-dependency-installtions

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,12 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/scripts'
-  ]
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/scripts',
+		'@pinia/nuxt',
+		'pinia-plugin-persistedstate/nuxt',
+		'nuxt-quasar-ui'
+	],
+
+	pinia: {
+		storesDirs: ['stores/**']
+	},
+
+	imports: {
+		dirs: ['composables', 'utils', 'types', 'services']
+	},
+
+	plugins: [],
+
+	quasar: {
+		plugins: ['Dialog', 'Loading', 'LoadingBar', 'Notify'],
+		extras: {
+			font: 'roboto-font'
+		},
+		config: {
+			notify: {
+				timeout: 2000,
+				progress: true,
+				actions: [
+					{
+						icon: 'close',
+						color: 'white',
+						round: true,
+						flat: true,
+						size: 'sm'
+					}
+				]
+			}
+		}
+	}
 })
