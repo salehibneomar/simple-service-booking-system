5b468986c10bebdd12ddc19f9e806f242d0130c5
salehibneomar <salehibneomar@gmail.com>
2025-07-29 16:45:58 +0600

    fixed the issue where unauth route was looking for the route named login but couldn't get since we have removed the login route, but we need to override the default since we created the auth.php, in laravel 12 there is no default authcontroller to change that, will look into it later

--- api/app/Exceptions/Handlers/AuthExceptionHandler.php
+++ api/app/Exceptions/Handlers/AuthExceptionHandler.php
@@ -4,13 +4,14 @@ namespace App\Exceptions\Handlers;
 
 use App\Traits\ApiResponser;
 use App\Enums\HttpStatus;
+use Illuminate\Auth\AuthenticationException;
 
 class AuthExceptionHandler
 {
     use ApiResponser;
 
-    public function handle(\Exception $exception)
+    public function handle(AuthenticationException $exception)
     {
-        return $this->errorResponse($exception, HttpStatus::UNAUTHORIZED, $exception->getMessage());
+        return $this->errorResponse($exception, HttpStatus::UNAUTHORIZED, 'Unauthenticated');
     }
 }
